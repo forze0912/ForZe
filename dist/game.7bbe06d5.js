9aeb852b4047b9d7afa52351c231bcc6221b54ed
forze0912 <86575115+forze0912@users.noreply.github.com>
2021-09-29 17:31:26 -1000

    wasd

--- dist/game.7bbe06d5.js
+++ dist/game.7bbe06d5.js
@@ -4140,7 +4140,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56948" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57763" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
